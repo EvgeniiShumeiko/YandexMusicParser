1a145281dd594c8a0a355eef7d5ad1fe282f6a6c
Evgenii Shumeiko <nicesay.ru@gmail.com>
2018-10-03 18:03:16 +0500

    some fix

--- modules/artists.py
+++ modules/artists.py
@@ -1,17 +1,20 @@
 import mysql.connector
-
+import json
 
 def save_artists_page(artist_list):
     for a in artist_list:
         artist = a["artist"]
 
+
         data = {
             'ym_artist_id': artist["id"],
             "name": artist["name"],
             "available": artist["available"],
-            "links": ', '.join(artist["links"]),
+            "links": json.dumps(artist["links"]),
             "genres": ', '.join(artist["genres"])
         }
+        print(artist)
+        break
 
         if check_artist(artist["id"]):
             update_user(artist["id"])
@@ -20,19 +23,20 @@ def save_artists_page(artist_list):
 
 
 def check_artist(id: int):
-    cnx = mysql.connector.connect(host='localhost', database='yandex_data', user='analytic',
-                                  password='CzeY3YOW9lzk71D3')
-    cursor = cnx.cursor()
-
-    query = ("SELECT id FROM artists "
-             "WHERE ym_artist_id = '%s'")
-
-    cursor.execute(query, id)
-
-    print(cursor)
-
-    cursor.close()
-    cnx.close()
+    pass
+    # cnx = mysql.connector.connect(host='localhost', database='yandex_data', user='analytic',
+    #                               password='CzeY3YOW9lzk71D3')
+    # cursor = cnx.cursor()
+    #
+    # query = ("SELECT id FROM artists "
+    #          "WHERE ym_artist_id = '%s'")
+    #
+    # cursor.execute(query, id)
+    #
+    # print(cursor)
+    #
+    # cursor.close()
+    # cnx.close()
 
 
 def update_user():
