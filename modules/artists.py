eeff3f5f31ee689171182f2380fb47099ff1f30f
Evgenii Shumeiko <nicesay.ru@gmail.com>
2018-10-03 18:04:48 +0500

    some fix

--- modules/artists.py
+++ modules/artists.py
@@ -13,10 +13,9 @@ def save_artists_page(artist_list):
             "links": json.dumps(artist["links"]),
             "genres": ', '.join(artist["genres"])
         }
-        print(artist)
-        break
 
         if check_artist(artist["id"]):
+            break
             update_user(artist["id"])
         else:
             insert_user(artist["id"])
@@ -24,19 +23,19 @@ def save_artists_page(artist_list):
 
 def check_artist(id: int):
     pass
-    # cnx = mysql.connector.connect(host='localhost', database='yandex_data', user='analytic',
-    #                               password='CzeY3YOW9lzk71D3')
-    # cursor = cnx.cursor()
-    #
-    # query = ("SELECT id FROM artists "
-    #          "WHERE ym_artist_id = '%s'")
-    #
-    # cursor.execute(query, id)
-    #
-    # print(cursor)
-    #
-    # cursor.close()
-    # cnx.close()
+    cnx = mysql.connector.connect(host='localhost', database='yandex_data', user='analytic',
+                                  password='CzeY3YOW9lzk71D3')
+    cursor = cnx.cursor()
+
+    query = ("SELECT id FROM artists "
+             "WHERE ym_artist_id = '%s'")
+
+    cursor.execute(query, id)
+
+    print(cursor)
+
+    cursor.close()
+    cnx.close()
 
 
 def update_user():
