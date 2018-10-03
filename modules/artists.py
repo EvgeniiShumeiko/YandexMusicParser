0b4c99e5d9c5dafac0b27f0d1f0afc19e91c7e1c
Evgenii Shumeiko <nicesay.ru@gmail.com>
2018-10-03 18:54:02 +0500

    fix once again

--- modules/artists.py
+++ modules/artists.py
@@ -8,11 +8,12 @@ def save_artists_page(artist_list):
         artist = a["artist"]
         data = {
             "name": artist["name"],
-            "available": artist["available"],
+            "available": 1 if artist["available"] else 0 ,
             "links": json.dumps(artist["links"]),
             "genres": ', '.join(artist["genres"]),
-            "date_update_parse": time.time()
+            "date_update_parse": int(time.time())
         }
+
 
         if check_artist(artist["id"]):
             update_user(artist["id"], data)
@@ -33,9 +34,7 @@ def check_artist(artist_id: int):
 def update_user(artist_id, data):
     connection = db.get_connection()
     with connection.cursor() as cursor:
-        keys = data.keys()
-        values = data.values()
-        kv_list = ["{0}='{1}'".format(k,v) for k,v in (keys, values)]
+        kv_list = ["{0}='{1}'".format(k, v) for k, v in data.items()]
         sql = "UPDATE `artists` SET %s WHERE ym_artist_id=%s"
         cursor.execute(sql, (", ".join(kv_list), artist_id))
         connection.commit()
