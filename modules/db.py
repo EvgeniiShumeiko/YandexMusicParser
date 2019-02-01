a1df95d35bd1776385fdf0e268d7480b743b16a8
EvgeniiShumeiko <nicesay.ru@gmail.com>
2019-02-01 18:20:53 +0500

    Update db.py

--- modules/db.py
+++ modules/db.py
@@ -3,8 +3,8 @@ import pymysql.cursors
 
 def get_connection():
     connection = pymysql.connect(host='localhost',
-                                 user='analytic',
-                                 password='CzeY3YOW9lzk71D3',
+                                 user='user',
+                                 password='pass',
                                  db='yandex_data',
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
